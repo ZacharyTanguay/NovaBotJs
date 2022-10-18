e1fbe1f4230aeda81d8d5e0289a52b5a163a1337
ZacharyTanguay <2176651@cegepmontpetit.ca>
2022-10-18 13:35:50 +0000

    commande amende terminer

--- commands/economy/amende.js
+++ commands/economy/amende.js
@@ -1,8 +1,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { EmbedBuilder, Colors, ButtonStyle, ButtonBuilder, ActionRowBuilder } = require("discord.js")
-const { User } = require("C:/NovaBotJs/utils/schema.js")
+const { EmbedBuilder, Colors, ButtonStyle, ButtonBuilder, ActionRowBuilder, ComponentType } = require("discord.js")
+const { User } = require("/workspace/NovaBotJs/utils/schema.js")
 const { v4: uuidv4 } = require('uuid');
-const conf = require("C:/NovaBotJs/config/embed.json")
+const conf = require("/workspace/NovaBotJs/config/embed.json")
 
 uuidv4();
 
@@ -36,8 +36,8 @@ module.exports = {
     run: async (interaction) => {
         const user = interaction.member.user
         const target = interaction.options.getUser("utilisateur")
-        amount = interaction.options.getNumber("montant")
         userData = await User.findOne({ id: user.id }) || new User({ id: user.id }),
+        amount = interaction.options.getNumber("montant")
         targetData = await User.findOne({ id: target.id }) || new User({ id: target.id })
 
         const amendeEmbed = new EmbedBuilder()
@@ -45,6 +45,7 @@ module.exports = {
         .setDescription(`**${target.username}** vous avez reçu une amende d'un montant s'élevant à **${formatter.format(amount)}** de **${user.username}**. \n\nLa raison de cette dernière :\n**${interaction.options.getString("raison")}** ! \n\n__Acceptez-vous de payer cette amende__ ?`)
         .setFooter({text: "🧾"})
 
+        let messageArray = []
         let message = await interaction.reply({embeds: [amendeEmbed], fetchReply: true})
 
         const accepter = new ButtonBuilder()
@@ -65,40 +66,43 @@ module.exports = {
             refuser
         )
         
-        interaction.editReply({embeds: [amendeEmbed], components: [row], fetchReply: true})
-        const collector = interaction.channel.createMessageComponentCollector({ time: 60000 });
-        collector.on('collect', async ButtonInteraction => {
-            
+        messageArray += message.id
+        messageArray.save
+
+        await interaction.editReply({embeds: [amendeEmbed], components: [row], fetchReply: true})
+        const collector = interaction.channel.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60000, errors: ['time'] });
+        collector.on('collect', async (ButtonInteraction) => {
             if (target.id != ButtonInteraction.user.id) {
                 const wrongUser = new EmbedBuilder()
                 .setColor(conf.avertissement)
                 .setDescription(`**${user.username}** cette amende ne vous est pas adressée !`)
                 .setFooter({text: "⚠️"})
 
-                interaction.followUp({ embeds:[wrongUser], components:[], ephemeral: true })
+                await interaction.editReply({ embeds:[wrongUser], components:[], ephemeral: true })
             }
 
             if (ButtonInteraction.customId == message.id + "accepter" && target.id == ButtonInteraction.user.id) {
                 await ButtonInteraction.deferUpdate();
+                amount = interaction.options.getNumber("montant")
                 const accepterEmbed = new EmbedBuilder()
                 .setColor(conf.accepter)
                 .setDescription(`**${user.username}** vous avez payé une amende de **${formatter.format(amount)}** à **${target.username}**`)
                 .setFooter({text: "✅"})
-
                 targetData.Cash -= amount
                 targetData.save()
                 
-                interaction.editReply({embeds: [accepterEmbed], components: []})
+                await interaction.editReply({embeds: [accepterEmbed], components: []})
             }
 
             if (ButtonInteraction.customId == message.id + "refuser" && target.id == ButtonInteraction.user.id) {
                 await ButtonInteraction.deferUpdate();
+                amount = interaction.options.getNumber("montant")
                 const refuserEmbed = new EmbedBuilder()
                 .setColor(conf.refuser)
                 .setDescription(`**${user.username}** vous avez refusé de payer une amende de **${formatter.format(amount)}** à **${target.username}**`)
                 .setFooter({text: "❌"})
 
-                interaction.editReply({embeds: [refuserEmbed], components: []})
+                await interaction.editReply({embeds: [refuserEmbed], components: []})
             }
         });
         collector.on('end', collected => {console.log(`Collected ${collected.size} items`); collector.stop()});
