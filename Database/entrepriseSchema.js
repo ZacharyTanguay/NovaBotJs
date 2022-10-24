b46322e671847d58730e2563d3fac8bc19e9f143
ZacharyTanguay <103211534+ZacharyTanguay@users.noreply.github.com>
2022-10-24 10:41:10 -0400

    Update entrepriseSchema.js

--- Database/entrepriseSchema.js
+++ Database/entrepriseSchema.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose")
 
+const msg = require("C:/NovaBotJs/BotJson/message.json")
+
+const Entreprise = new mongoose.Schema({
+    liquide: { type: Number, default: 0 },
+    banque: { type: Number, default: 0 },
+    cryptomonnaie: { type: Number, default: 0 },
+    argent_sale: { type: Number, default: 0 },
+    argent_blanchi: { type: Number, default: 0 }
+})
