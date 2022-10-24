63b678582608d61f4d7e1317c7b724d2f644287e
ZacharyTanguay <103211534+ZacharyTanguay@users.noreply.github.com>
2022-10-24 11:13:57 -0400

    Update statsSchema.js

--- Database/statsSchema.js
+++ Database/statsSchema.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose")
+const msg = require("C:/NovaBotJs/BotJson/message.json")
+const { moneyFormat } = require("C:/NovaBotJs/Handlers/functions.js")
 
+const Statistiques = new mongoose.Schema({
+    stats_nbUser: { type: Number, default : 0 },
+    stats_nbCitoyen: { type: Number, default: 0 },
+    stats_nbEntreprise: { type: Number, default: 0 },
+    stats_argentLiquide: { type: String, default: "0" },
+    stats_argentBanque: { type: String, default: "0" },
+    stats_argentSale: { type: String, default: "0" },
+    stats_cryptomonnaie: { type: String, default: "0" },
+})
