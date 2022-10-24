03c0cf8be7d0a32a7bd9df12f87a6ea8eb8eccaf
ZacharyTanguay <103211534+ZacharyTanguay@users.noreply.github.com>
2022-10-24 14:56:15 -0400

    Update citoyenSchema.js

--- Database/citoyenSchema.js
+++ Database/citoyenSchema.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose")
 const msg = require("C:/NovaBotJs/BotJson/message.json")
 
 const Citoyen = new mongoose.Schema({
-    id: { type: String, unique: true, required: true },
+    user_id: { type: String, unique: true, required: true },
     
     carte_identite_vrai: { type: Boolean, default: false },
     carte_identite_fausse: { type: String, default: false },
     compte_bancaire: { type: Boolean, default: false },
-    entreprise: { type: Boolean, default: false },
 
     liquide: { type: Number, default: 0 },
     banque: { type: Number, default: 0 },
@@ -15,12 +14,16 @@ const Citoyen = new mongoose.Schema({
     argent_sale: { type: Number, default: 0 },
     argent_blanchi: { type: Number, default: 0 },
 
-    vrai_nom: { type: String, default: "Non rempli" },
-    vrai_prenom: { type: String, default: "Non rempli" },
-    vrai_age: { type: String, default: "Non rempli" },
-    vrai_sexe: { type: String, default: "Non rempli" },
-    vrai_naissance: { type: String, default: "Non rempli" },
-    vrai_nationalite: { type: String, default: "Non rempli" },
+    vrai_id: [
+        new Schema({
+            vrai_nom: { type: String, default: "Non rempli" },
+            vrai_prenom: { type: String, default: "Non rempli" },
+            vrai_age: { type: String, default: "Non rempli" },
+            vrai_sexe: { type: String, default: "Non rempli" },
+            vrai_naissance: { type: String, default: "Non rempli" },
+            vrai_nationalite: { type: String, default: "Non rempli" },
+        }, {_id: false})
+    ],
 
     faux_nom: { type: String, default: "Non rempli" },
     faux_prenom: { type: String, default: "Non rempli" },
