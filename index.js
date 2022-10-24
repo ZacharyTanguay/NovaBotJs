c4e0a35e60d503a54630b837252acfe4962c0e8d
Zack <2176651@cegepmontpetit.ca>
2022-10-23 20:16:00 -0400

    feat: index.js code is working.

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { bot_token, mongo_url } = require('./config.json');
+const {loadEvents} = require("./handlers/eventHandler");
+const {loadCommands} = require("./handlers/commandHandler");
+
+const mongoose = require('mongoose');
+const { loadButtons } = require('./events/interactions/interactionCreate');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+client.commands = new Collection();
+client.events = new Collection();
+
+client.login(bot_token)
+.then(() => {
+	loadCommands(client);
+	loadEvents(client);
+	mongoose.connect(mongo_url)
+})
+.catch((err) => console.log(err));
