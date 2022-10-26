205058c35bc6eb1418370e1f7e2ee541cb74abdb
ZacharyTanguay <103211534+ZacharyTanguay@users.noreply.github.com>
2022-10-26 08:38:47 -0400

    Update userSchema.js

--- Database/userSchema.js
+++ Database/userSchema.js
@@ -3,10 +3,20 @@ const msg = require("C:/NovaBotJs/BotJson/message.json")
 
 const User = new mongoose.Schema({
     user_id: { type: String, unique: true, required: true },
+    user_tag: { type: String, default: "Anonyme" },
     user_nom: { type: String, default: "Anonyme" },
     user_prenom: { type: String, default: "Anonyme" },
     user_age: { type: String, default: "Anonyme" },
     user_sexe: { type: String, default: "Anonyme" },
-    user_discord: { type: String, default: "Anonyme" },
     user_gamertag: { type: String, default: "Anonyme" },
+    
+    user_warn: [
+        {
+           tag : { type: String, default: "" },
+           warning: { type: String, default: "" },
+           raison: { type: String, default:  "" },
+           gravité: { type: Number, default: 0 },
+           moderateur: { type: String, default "@Staff" },
+        }
+    ],
 })
