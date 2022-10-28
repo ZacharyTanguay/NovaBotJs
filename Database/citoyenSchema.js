347f9afb1eb79945d299e91b3c6c9e1d976a43a6
ZacharyTanguay <103211534+ZacharyTanguay@users.noreply.github.com>
2022-10-28 12:15:38 -0400

    Update citoyenSchema.js

--- Database/citoyenSchema.js
+++ Database/citoyenSchema.js
@@ -15,25 +15,25 @@ const Citoyen = new mongoose.Schema({
     argent_blanchi: { type: Number, default: 0 },
 
     vrai_id: [
-        new Schema({
+        {
             nom: { type: String, default: "" },
             prenom: { type: String, default: "" },
             age: { type: String, default: "" },
             sexe: { type: String, default: "" },
             naissance: { type: String, default: "" },
             nationalite: { type: String, default: "" },
-        }, {_id: false})
+        }
     ],
     
     faux_id: [
-        new Schema({
+        {
             nom: { type: String, default: "" },
             prenom: { type: String, default: "" },
             age: { type: String, default: "" },
             sexe: { type: String, default: "" },
             naissance: { type: String, default: "" },
             nationalite: { type: String, default: "" },
-        }, {_id: false})
+        }
     ],
     
     permis_conduire_voiture: { type: String, default: msg.permis_deny_emoji },
